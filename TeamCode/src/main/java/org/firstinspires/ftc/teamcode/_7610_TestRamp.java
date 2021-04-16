129ff5e6f0613df75a6fa63d9ee044ba711b8827
Sophia <sophiayuna@gmail.com>
2021-04-16 14:15:22 -0700

    fixed ramp issue

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_7610_TestRamp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/_7610_TestRamp.java
@@ -145,7 +145,7 @@ public class _7610_TestRamp extends OpMode
          */
 
         if ((analog.getVoltage() <= 0.58 && gamepad1.right_stick_y > 0) || (analog.getVoltage() >= 0.235 && gamepad1.right_stick_y < 0))
-            rampPower = Range.clip(gamepad1.right_stick_y, -1, 1);
+            rampPower = Range.clip(gamepad2.right_stick_y, -1, 1);
         else rampPower = 0;
 
         ramp.setPower(rampPower);
